ab3e2de348ad7f93848f1ab359ef026362709a50
fjserrano01 <43230027+fjserrano01@users.noreply.github.com>
2020-12-07 03:01:27 -0700

    Fixing archive paths

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -38,7 +38,7 @@ export const updateSystem = (id, payload)=> api3.put(`/system/${id}`, payload)
 export const updateSystemArchive = (id, payload)=> api3.put(`/systemarch/${id}`, payload)
 export const deleteSystem = id => api3.delete(`/system/${id}`)
 export const getSystemById = id => api3.get(`/system/${id}`)
-export const getArchivedSystems = id => api3.get(`/system/${id}`)
+export const getArchivedSystems = () => api3.get(`/system/`)
 
 export const insertSubtask = payload => api4.post(`/subtask`, payload)
 export const getAllSubtask = () => api4.get(`/subtasks`)
@@ -47,7 +47,7 @@ export const updateSubtaskArchive = (id, payload)=> api4.put(`/subtaskarch/${id}
 export const deleteSubtask = id => api4.delete(`/subtask/${id}`)
 export const getSubtaskById = id => api4.get(`/subtask/${id}`)
 export const getSubtaskByTask = id => api4.get(`/subtasktask/${id}`)
-export const getArchivedSubtasks = id => api4.get(`/subtask/${id}`)
+export const getArchivedSubtasks = () => api4.get(`/subtask/`)
 
 
 export const insertTask = payload => api5.post(`/task`, payload)
@@ -59,7 +59,7 @@ export const updateTaskArchive = (id, payload)=> api5.put(`/taskarch/${id}`, pay
 export const deleteTask = id => api5.delete(`/task/${id}`)
 export const getTaskById = id => api5.get(`/task/${id}`)
 export const getTaskBySystem = id => api5.get(`/tasksystem/${id}`)
-export const getArchivedTasks = id => api5.get(`/task/${id}`)
+export const getArchivedTasks = () => api5.get(`/task/`)
 
 export const insertFinding = payload => api6.post(`/finding`, payload)
 export const getAllFindings= () => api6.get(`/findings`)
@@ -67,7 +67,7 @@ export const updatefinding = (id, payload)=> api6.put(`/finding/${id}`, payload)
 export const updatefindingarchive = (id, payload)=> api6.put(`/findingarch/${id}`, payload)
 export const deleteFinding = id => api6.delete(`/finding/${id}`)
 export const getFindingById = id => api6.get(`/finding/${id}`)
-export const getArchivedFindings = id => api6.get(`/finding/${id}`)
+export const getArchivedFindings = () => api6.get(`/finding/`)
 
 
 const apis = {
