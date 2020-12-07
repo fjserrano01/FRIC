bd7ea15ec3099a888555a5705f82e3183b5d1693
fjserrano01 <43230027+fjserrano01@users.noreply.github.com>
2020-12-07 00:53:43 -0700

    delting reports, directory structure

--- reportsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const reportsCtrl = require('../controllers/reports-ctrl')
-const router = express.Router()
-
-router.get('/risk',reportsCtrl.riskMatrixReport)
-router.get('/erb',reportsCtrl.ERBReport)
-router.get('/final', reportsCtrl.finalReport)
-
-module.exports = router
